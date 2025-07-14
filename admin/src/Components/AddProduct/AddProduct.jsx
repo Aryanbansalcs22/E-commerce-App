c44ff8e904230aa2b181e8d7fa0dd8482f0cdbd0
Aryan Bansal <157011307+Aryanbansalcs22@users.noreply.github.com>
2025-07-14 20:26:16 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -28,7 +28,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product',image);
 
-        await fetch('https://e-commerce-app-backend-73bp.onrender.com/',{
+        await fetch('https://e-commerce-app-backend-73bp.onrender.com/upload',{
             method: 'POST',
              headers: {
                 Accept: 'application/json'
@@ -39,7 +39,7 @@ const AddProduct = () => {
         if(responseData.success){
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('https://e-commerce-app-backend-73bp.onrender.com/',{
+            await fetch('https://e-commerce-app-backend-73bp.onrender.com/addproduct',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
