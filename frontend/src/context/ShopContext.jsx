30dbcc728d2e13b05c082901ea48add7da431f16
Aryan Bansal <157011307+Aryanbansalcs22@users.noreply.github.com>
2025-07-14 19:18:32 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -16,12 +16,12 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(()=>{
-    fetch('http://localhost:4000/allproducts')
+    fetch('https://e-commerce-app-backend-73bp.onrender.com/')
     .then((response)=>response.json())
     .then((data)=>setAll_Product(data))
     
     if (localStorage.getItem('auth-token')) {
-      fetch('http://localhost:4000/getcart', {
+      fetch('https://e-commerce-app-backend-73bp.onrender.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -43,7 +43,7 @@ const ShopContextProvider = (props) => {
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     if (localStorage.getItem('auth-token')) {
-      fetch('http://localhost:4000/addtocart', {
+      fetch('https://e-commerce-app-backend-73bp.onrender.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -77,7 +77,7 @@ const ShopContextProvider = (props) => {
     });
   
     if (localStorage.getItem('auth-token')) {
-      fetch('http://localhost:4000/removefromcart', {
+      fetch('https://e-commerce-app-backend-73bp.onrender.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
