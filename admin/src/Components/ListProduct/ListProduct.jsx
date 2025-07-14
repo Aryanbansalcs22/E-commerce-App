ecb189fd3d45da55d83950fb446c08de15e9d75b
Aryan Bansal <157011307+Aryanbansalcs22@users.noreply.github.com>
2025-07-14 20:27:44 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
     const [allproducts,setAllProducts] = useState([]);
 
     const fetchInfo = async()=>{
-        await fetch('https://e-commerce-app-backend-73bp.onrender.com/')
+        await fetch('https://e-commerce-app-backend-73bp.onrender.com/allproducts')
         .then((res)=>res.json())
         .then((data)=>{setAllProducts(data)});
     }
@@ -15,7 +15,7 @@ const ListProduct = () => {
     },[])
 
     const remove_product = async(id) =>{
-        await fetch('https://e-commerce-app-backend-73bp.onrender.com/',{
+        await fetch('https://e-commerce-app-backend-73bp.onrender.com/removeproduct',{
             method:'POST',
             headers:{
                 Accept:'application/json',
